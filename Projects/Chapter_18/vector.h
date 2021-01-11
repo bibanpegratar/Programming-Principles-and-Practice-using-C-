3c1555fc4bd7b0da91b5dd06e3101a8034e9011e
turbocarfastspeed313 <turbo_car_fast_speed313@protonmail.com>
2021-01-11 11:33:23 +0200

    RAII for vector implemented

--- Projects/Chapter_18/vector.h
+++ Projects/Chapter_18/vector.h
@@ -3,45 +3,51 @@
 //	- allows initialization for type T that doesnt have a default constructor, by letting the user specify the default value
 //But does it make sense to provide this ? The user that defined T should provide a default constructor for that class if he wants to use it with a vector anyway, so there's 
 //no point in doing all of this, except that classes with no default constructors cant be used to initialize a vector (except with {} notation )
+
+//The vector_base class makes everything a bit more complicated to read, but we ensure RAII for vector
+//Another solution would be to use smart pointers, will try that later
 #include <initializer_list>
 #include <algorithm>
 #include <memory>
 
 struct out_of_range {};
 
-template<typename T, typename A = std::allocator<T>> //requires Element<T>()
-class vector
+template<typename T, typename A>
+struct vector_base
 {
-	A alloc;
-	int sz; //number of elements in the vector
-	T* elem;
-	int space; //the maximum number of elements the vector can hold
+	A alloc; 	//allocator
+	T* elem; 	//start of allocation
+	int sz;  	//number of elements
+	int space;  //amount of allocated space
+	vector_base(int n)
+		:elem{alloc.allocate(n)}, sz{n}, space{n} {}
 
+	void construct(T def) {for (int i = 0; i < sz; i++) alloc.construct(&elem[i], def); }
+
+	~vector_base() {alloc.deallocate(elem, space); }
+};
+
+template<typename T, typename A = std::allocator<T>> //requires Element<T>()
+class vector : public vector_base<T,A>
+{
 	void reserve(int newspace);
 
 public:
 	
-	//constructors
-	vector() : sz{0}, elem{nullptr}, space{0} {}
-
-	vector (int size, T def = T()) :sz{size}, elem{alloc.allocate(size)}, space{size}
+	vector (int size, T def = T())
+		: vector_base<T,A>(size)
 	{
-		for (int i = 0; i < size; i++) alloc.construct(&elem[i], def); // initialize each element to 0		
+		this->construct(def); // initialize each element to 0		
 	}
 
 	//{} initialization
  	vector (std::initializer_list<int> lst)
-		:sz{lst.size()}, elem{alloc.allocate(lst.size())}, space{lst.size()}
+		:vector_base<T,A>(lst.size())
 	{
-		std::copy (lst.begin(), lst.end(), elem);
+		std::copy (lst.begin(), lst.end(), this->elem);
 	}
 
-	//destructor
-	~vector()
-	{
-		for(int i = 0; i < sz; i++) alloc.destroy(&elem[i]);
-		alloc.deallocate(elem, space);
-	}
+	//destruction of elements is managed in vector_base
 
  	vector (const vector& arg); //copy constructor
 	vector& operator= (const vector& arg); //copy assignment
@@ -50,14 +56,14 @@ public:
 	vector& operator= (vector&& arg); //move assignment
 
 	//subscript operators
-	T& operator[] (int n) { return elem[n]; }
-	const T& operator[] (int n) const { return elem[n]; }
+	T& operator[] (int n) { return this->elem[n]; }
+	const T& operator[] (int n) const { return this->elem[n]; }
 
     T& at(int n);
     const T& at(int n) const;
 
-	int size() const { return sz; }
-	int capacity() const { return space; }
+	int size() const { return this->sz; }
+	int capacity() const { return this->space; }
 
 	void resize(int newspace, T def = T()); //growth
 	void push_back(const T& d);
@@ -67,9 +73,10 @@ public:
 //copy constructor
 template<typename T,typename A> 
 vector<T,A>::vector (const vector& arg)
-	:sz{arg.sz}, elem{new T[sz]}
 {
-	std::copy (arg.elem, arg.elem + arg.sz, elem);
+	this->sz = arg.sz;
+	this->elem = new T[arg.size()];
+	std::copy (arg.elem, arg.elem + arg.sz, this->elem);
 }
 
 //copy assignment
@@ -78,41 +85,42 @@ vector<T,A>& vector<T,A>::operator= (const vector& arg)
 {
 	if (this == &arg) return *this; //self-assignment, do nothing
 
-	if (arg.sz <= space) // enough space, no need for extra allocation
+	if (arg.sz <= this->space) // enough space, no need for extra allocation
 	{
-		for (int i = 0; i < arg.sz; i++) elem[i] = arg.elem[i]; //copy elements
-		sz = arg.sz;
+		for (int i = 0; i < arg.sz; i++) this->elem[i] = arg.elem[i]; //copy elements
+		this->sz = arg.sz;
 		return *this;
 	}
 
 	T* n = new T[arg.sz]; 
 	for (int i = 0; i < arg.sz; i++) n[i] = arg.elem[i];  //copy all the elements from a to the newly allocated array
-	delete[] elem; //delete the previous value held by elem (remember that it's an ASSIGNMENT, not an INITIALIZATION)
-	space = arg.sz;
-	sz = arg.sz;
-	elem = n; //set the elem (of the current vector) to the argumen's elem
+	delete[] this->elem; //delete the previous value held by elem (remember that it's an ASSIGNMENT, not an INITIALIZATION)
+	this->space = arg.sz;
+	this->sz = arg.sz;
+	this->elem = n; //set the elem (of the current vector) to the argumen's elem
 	return *this; //return a self-reference
 }
 
 template<typename T, typename A>
 T& vector<T,A>::at(int n)
 {
-    if (n < 0 || n >= sz) throw out_of_range();
-    return elem[n];
+    if (n < 0 || n >= this->sz) throw out_of_range();
+    return this->elem[n];
 }
 
 template<typename T, typename A>
 const T& vector<T,A>::at(int n) const
 {
-    if (n < 0 || n >= sz) throw out_of_range();
-    return elem[n];  
+    if (n < 0 || n >= this->sz) throw out_of_range();
+    return this->elem[n];  
 }
 
 //move constructor
 template<typename T,typename A> 
 vector<T,A>::vector (vector&& arg)
-	:sz{arg.sz}, elem{arg.elem}
 {
+	this->sz = arg.sz;
+	this->elem = arg.elem;
 	arg.sz = 0;
 	arg.elem = nullptr;
 }
@@ -121,9 +129,9 @@ vector<T,A>::vector (vector&& arg)
 template<typename T,typename A> 
 vector<T,A>& vector<T,A>::operator= (vector&& arg)
 {
-	delete[] elem;
-	elem = arg.elem;
-	sz = arg.sz;
+	delete[] this->elem;
+	this->elem = arg.elem;
+	this->sz = arg.sz;
 	arg.elem = nullptr;
 	arg.sz = 0;
 	return *this;
@@ -133,13 +141,13 @@ vector<T,A>& vector<T,A>::operator= (vector&& arg)
 template<typename T,typename A> 
 void vector<T,A>::reserve(int newspace)
 {
-	if(newspace <= space) return; //never allocate less memory
-	T* p = alloc.allocate(newspace);
-	for(int i = 0; i < sz; i++) alloc.construct(&p[i], elem[i]); //copy the elements from the old array to the new one
-	for(int i = 0; i < sz; i++) alloc.destroy(&elem[i]);
-	alloc.deallocate(elem, space);
-	elem = p; //point elem to the newly allocated array p
-	space = newspace;
+	if(newspace <= this->space) return; //never allocate less memory
+	T* p = this->alloc.allocate(newspace);
+	for(int i = 0; i < this->sz; i++) this->alloc.construct(&p[i], this->elem[i]); //copy the elements from the old array to the new one
+	for(int i = 0; i < this->sz; i++) this->alloc.destroy(&this->elem[i]);
+	this->alloc.deallocate(this->elem, this->space);
+	this->elem = p; //point elem to the newly allocated array p
+	this->space = newspace;
 }
 
 template<typename T,typename A> 
@@ -150,19 +158,19 @@ void vector<T,A>::resize(int newspace, T def)
 //	-or a value d if specified
 {
 	reserve(newspace);
-	for (int i = sz; i < newspace; i++) alloc.construct(&elem[i],def);
-	sz = newspace;
+	for (int i = this->sz; i < newspace; i++) this->alloc.construct(&this->elem[i],def);
+	this->sz = newspace;
 }
 
 template<typename T,typename A> 
 void vector<T,A>::push_back(const T& d)
 {
-	if(space == 0) //if there's no space (default constructor)
+	if(this->space == 0) //if there's no space (default constructor)
 		reserve(8); //reserve space for 8 elements
 
-	else if (sz == space) //if the space is equal to the size
-		reserve(space * 2); //double the space
+	else if (this->sz == this->space) //if the space is equal to the size
+		reserve(this->space * 2); //double the space
 
-	alloc.construct(&elem[sz], d); // add d at the end
-	++sz; // increase size by 1
+	this->alloc.construct(&this->elem[this->sz], d); // add d at the end
+	++this->sz; // increase size by 1
 }
